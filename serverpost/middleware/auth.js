3e6203b19e45a40a23320cc6bb92415250e8d7bd
amalraj-g <g.amalraj1993@gmail.com>
2023-04-18 21:55:23 +0530

    routine process

--- serverpost/middleware/auth.js
+++ serverpost/middleware/auth.js
@@ -1,37 +1,27 @@
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
-import {  unAuthorized, serverError } from '../constants/values.js';
 dotenv.config();
+import {  unAuthorized, serverError } from '../constants/values.js';
 
 const auth = async ( req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
-       
-        if (token){ 
+        if(token){ 
             jwt.verify(token, process.env.SECRET,(err,decoded)=>{
-                if(err){
-                    throw new Error('jsonWebTokenError');
-                } else{
-                    
+                if(err) throw new Error('jsonWebTokenError');
+                else{
                     req.userId = decoded?.id;
-                    next();
-                }
+                    next();}
             });
         }
         else {
             let decodedData = jwt.decode(token);
             req.userId = decodedData?.sub;
-                    
         }
     } catch (error) {
-        if(error.message ==='jsonWebTokenError'){
-            next({status:unAuthorized, message:'invalid token'});
-        }else{
-            next({status:serverError, message:'internal serverError generated'});
-        }
-        
+        if(error.message ==='jsonWebTokenError') next({status:unAuthorized, message:'invalid token'});
+        else next({status:serverError, message:'internal serverError generated'});
     }
-
 };
     
 export default auth;
